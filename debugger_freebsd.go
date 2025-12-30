b159b8f7e2e1d872250d0d67228125578701f2e3
Alistair Mackay <34012094+fireflycons@users.noreply.github.com>
2025-12-30 14:10:39 +0000

    Parse ps output

--- debugger_freebsd.go
+++ debugger_freebsd.go
@@ -3,26 +3,44 @@
 package debugger
 
 import (
-	"syscall"
-
-	"golang.org/x/sys/unix"
+	"bufio"
+	"bytes"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
 )
 
-// From <sys/ptrace.h> on FreeBSD
-const ptDenyAttach = 31
-
 func isBeingDebugged() bool {
-	// ptrace(PT_DENY_ATTACH, 0, NULL, 0)
-	_, _, errno := syscall.Syscall6(
-		unix.SYS_PTRACE,
-		uintptr(ptDenyAttach),
-		0,
-		0,
-		0,
-		0,
-		0,
-	)
+	// No easy way to walk the process heirarchy with process names
+	// other than to parse the output of ps.
+	cmd := exec.Command("ps", "-o", "pid,ppid,comm", "-ax")
+	out, err := cmd.Output()
+	if err != nil {
+		return false
+	}
 
-	// If already being traced, kernel returns EPERM
-	return errno == unix.EPERM
+	pid := os.Getpid()
+	parents := map[int]int{}
+	names := map[int]string{}
+
+	scanner := bufio.NewScanner(bytes.NewReader(out))
+	scanner.Scan() // skip header
+
+	for scanner.Scan() {
+		var p, pp int
+		var name string
+		fmt.Sscanf(scanner.Text(), "%d %d %s", &p, &pp, &name)
+		parents[p] = pp
+		names[p] = name
+	}
+
+	for pid != 0 {
+		if names[pid] == "dlv" || strings.HasPrefix(names[pid], "gdb") {
+			return true
+		}
+		pid = parents[pid]
+	}
+
+	return false
 }

87ee3b00fb54cc67fcd04a8172fe3f94601a3307
Alistair Mackay <34012094+fireflycons@users.noreply.github.com>
2025-12-31 07:08:12 +0000

    Darwin same as BSD

--- debugger_darwin.go
+++ debugger_darwin.go
@@ -1,25 +1,50 @@
-// debugger/debugger_darwin.go
 //go:build darwin
 
 package debugger
 
 import (
+	"bufio"
+	"bytes"
 	"context"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
 	"time"
-
-	"golang.org/x/sys/unix"
 )
 
-// From sys/proc.h on Darwin
-const pTraced = 0x000008
-
 func isBeingDebugged() bool {
-	info, err := unix.SysctlKinfoProc("kern.proc.pid", unix.Getpid())
+	// No easy way to walk the process heirarchy with process names
+	// other than to parse the output of ps.
+	cmd := exec.Command("ps", "-o", "pid,ppid,comm", "-ax")
+	out, err := cmd.Output()
 	if err != nil {
 		return false
 	}
 
-	return info.Proc.P_flag&pTraced != 0
+	pid := os.Getpid()
+	parents := map[int]int{}
+	names := map[int]string{}
+
+	scanner := bufio.NewScanner(bytes.NewReader(out))
+	scanner.Scan() // skip header
+
+	for scanner.Scan() {
+		var p, pp int
+		var name string
+		fmt.Sscanf(scanner.Text(), "%d %d %s", &p, &pp, &name)
+		parents[p] = pp
+		names[p] = name
+	}
+
+	for pid != 0 {
+		if names[pid] == "dlv" || strings.HasPrefix(names[pid], "gdb") {
+			return true
+		}
+		pid = parents[pid]
+	}
+
+	return false
 }
 
 func poll(context.Context, time.Duration) {
